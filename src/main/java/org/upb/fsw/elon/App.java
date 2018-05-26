4eaccba324a7ad79a2a5c845e0d8c1bd17a50755
bszabo <bszabo@wenetto.hu>
2018-05-26 15:43:25 +0200

    added pom and init

---
+++ src/main/java/org/upb/fsw/elon/App.java
@@ -0,0 +1,38 @@
+package org.upb.fsw.elon;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.aksw.qa.annotation.spotter.ASpotter;
+import org.aksw.qa.annotation.spotter.Fox;
+import org.aksw.qa.annotation.spotter.Spotlight;
+import org.aksw.qa.commons.datastructure.Entity;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.impl.ResourceImpl;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		Set<RDFNode> systemAnswers = new HashSet();
+		
+		systemAnswers.add(new ResourceImpl("true"));
+		
+		for (ASpotter m : new ASpotter[] { new Spotlight() }) {
+			Map<String, List<Entity>> ents = m.getEntities("What is a pencil?");
+			if (!ents.isEmpty()) {
+				System.out.println("Succes. Found " + ents.size() + " entities.");
+				
+				for (Entity ent : ents.get("en")) {
+					System.out.println(ent.toString());
+				}
+			} else {
+				System.out.println("No entities found.");
+			}
+		}
+		
+	}
+
+}
