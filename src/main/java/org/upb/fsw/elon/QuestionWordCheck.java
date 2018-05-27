beafedcdd56c3366976a7504b3b50175910e0ee5
Juan Drets <jdrets@github.com>
2018-05-27 12:34:01 +0200

    Question word check

---
+++ src/main/java/org/upb/fsw/elon/QuestionWordCheck.java
@@ -0,0 +1,24 @@
+package org.upb.fsw.elon;
+import java.io.*;
+import org.openrdf.query.algebra.Str;
+
+public class QuestionWordCheck {
+	
+	public static int SearchQuestionWord(String str) {
+		String [] questionWords = { "who", "where", "why", "what", "when", "how" };
+		String [] arrayOfWords = str.split(":");  
+		
+		for (int i = 0; i < arrayOfWords.length; i++) {
+			for (int j = 0; j < questionWords.length; j++) {
+				if (arrayOfWords[i] == questionWords[j]) {
+					return j;
+				}
+			}
+		}
+		return 0;
+	}
+	
+	public static void main(String args) {
+		SearchQuestionWord(args);
+	}
+}
