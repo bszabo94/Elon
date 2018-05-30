9bff9ca95c757179337ab5da2c30bf81046c5f51
Bence Szabó <bszabo@wenetto.hu>
2018-05-30 22:59:32 +0200

    added proper exception

---
+++ src/main/java/org/upb/fsw/elon/UnableToAnswerException.java
@@ -0,0 +1,13 @@
+package org.upb.fsw.elon;
+
+public class UnableToAnswerException extends Exception {
+		
+	public UnableToAnswerException() {
+		
+	};
+	
+	public UnableToAnswerException(String message) {
+		super(message);
+	}
+
+}
