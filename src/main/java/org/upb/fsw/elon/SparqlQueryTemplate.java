52af48df2f624feed7f8f2ae9614235d46d1f413
Bence Szabó <bszabo@wenetto.hu>
2018-06-14 14:55:06 +0200

    added build qery method

--- src/main/java/org/upb/fsw/elon/SparqlQueryTemplate.java
+++ src/main/java/org/upb/fsw/elon/SparqlQueryTemplate.java
@@ -1,5 +1,29 @@
 package org.upb.fsw.elon;
 
+import java.util.List;
+
 public class SparqlQueryTemplate {
+	
+	private String query;
+	
+	public SparqlQueryTemplate(String query) {
+		this.query = query;
+	}
+	
+	public String buildQueryString(List<String> parameters) {
+		
+		String finalQuery = query;
+		
+		for(String param: parameters) {
+			int pos = finalQuery.indexOf("%s");
+			String left = finalQuery.substring(0, pos + 2);
+			String right = finalQuery.substring(pos+2, finalQuery.length());
+			left = String.format(left, param);
+			finalQuery = left + right;
+		}
+		
+		return finalQuery;
+	}
+	
 
 }
