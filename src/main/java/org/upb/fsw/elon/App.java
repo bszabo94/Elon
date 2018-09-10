25d0d0d927b3db2d1dfe624e4b013eef673ad89e
bszabo <bszabo@wenetto.hu>
2018-09-10 23:24:41 +0200

    included webserver

--- src/main/java/org/upb/fsw/elon/App.java
+++ src/main/java/org/upb/fsw/elon/App.java
@@ -9,6 +9,8 @@ import org.aksw.qa.commons.datastructure.IQuestion;
 import org.aksw.qa.commons.load.Dataset;
 import org.aksw.qa.commons.load.LoaderController;
 
+import io.javalin.Javalin;
+
 /**
  *Just a class to hold the main function for testing the Elon class
  */
@@ -17,7 +19,7 @@ public class App {
 	public static void main(String[] args) {
 
 		
-		Elon elon = Elon.getInstance();
+		/*Elon elon = Elon.getInstance();
 		//loads both the qald8 train and test questions
 		List<IQuestion> testquestions = LoaderController.load(Dataset.QALD8_Test_Multilingual);
 		List<IQuestion> trainquestions = LoaderController.load(Dataset.QALD8_Train_Multilingual);
@@ -29,7 +31,14 @@ public class App {
 			elon.ask(trainquestions, "elon_train_" + date + "_" + time + ".json");
 		}catch (Exception e) {
 			e.printStackTrace();
-		}
+		}*/
+		
+		Javalin app = Javalin.create().start(7000);
+		app.post("/", ctx -> {
+			String query = ctx.formParam("query"),
+					lang = ctx.formParam("lang");
+			ctx.result("You asked \"" + query + "\" in " + lang + ".");	
+		});
 	}
 	
 }
